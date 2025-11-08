101cae6d540bf302377873476186c7f90f4c76ba
Xorgon <norman.reggox@gmail.com>
2025-11-08 14:29:39 -0500

    Fixed potential source of errors.

--- src/main/java/me/xorgon/volleyball/effects/BallLandEffect.java
+++ src/main/java/me/xorgon/volleyball/effects/BallLandEffect.java
@@ -3,6 +3,7 @@ package me.xorgon.volleyball.effects;
 import de.slikey.effectlib.Effect;
 import de.slikey.effectlib.EffectManager;
 import de.slikey.effectlib.EffectType;
+import de.slikey.effectlib.util.CustomSound;
 import de.slikey.effectlib.util.RandomUtils;
 import me.xorgon.volleyball.objects.Court;
 import org.bukkit.Color;
@@ -19,6 +20,8 @@ public class BallLandEffect extends Effect {
     private int count;
     private int particles = 50;
     private Court.Team scoringTeam;
+    private CustomSound landingBlastSound = new CustomSound(Sound.ENTITY_FIREWORK_ROCKET_BLAST);
+    private CustomSound landingLargeBlastSound = new CustomSound(Sound.ENTITY_FIREWORK_ROCKET_LARGE_BLAST);
 
     public BallLandEffect(EffectManager effectManager, Court court, Court.Team scoringTeam) {
         super(effectManager);
@@ -36,16 +39,20 @@ public class BallLandEffect extends Effect {
         Color color = scoringTeam == Court.Team.RED ? Color.RED : Color.BLUE;
         Location location = this.getLocation();
         for (int i = 0; i < particles; i++) {
-            Vector vector = RandomUtils.getRandomVector().multiply(count/7.5);
+            Vector vector = RandomUtils.getRandomVector().multiply(count / 7.5);
             location.add(vector);
             display(Particle.DUST, location, color);
             location.subtract(vector);
         }
-        if (new Random().nextDouble() < 0.2){ // Random low frequency.
-            if (Math.pow(new Random().nextDouble(), 2) * iterations/30 > 0.25) {
-                location.getWorld().playSound(location, Sound.ENTITY_FIREWORK_ROCKET_LARGE_BLAST, 2F, 0.5F);
+        if (new Random().nextDouble() < 0.2) { // Random low frequency.
+            if (Math.pow(new Random().nextDouble(), 2) * iterations / 30 > 0.25) {
+                landingLargeBlastSound.setVolume(2F);
+                landingLargeBlastSound.setPitch(0.5F);
+                landingLargeBlastSound.play(effectManager.getOwningPlugin(), location);
             } else {
-                location.getWorld().playSound(location, Sound.ENTITY_FIREWORK_ROCKET_BLAST, 2F, 0F);
+                landingBlastSound.setVolume(2F);
+                landingBlastSound.setPitch(0F);
+                landingBlastSound.play(effectManager.getOwningPlugin(), location);
             }
         }
         count++;
